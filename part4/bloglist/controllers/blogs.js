9336f84948659affd64d86c1a2acc95fb09390c8
tnhilanne <nikolas.developer@outlook.com>
2026-01-31 17:51:28 +0200

    Exercise 4.10

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -12,7 +12,7 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 // POST a new blog
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
   const blog = new Blog({
@@ -22,11 +22,8 @@ blogsRouter.post('/', (request, response, next) => {
     likes: body.likes || 0,
   })
 
-  blog.save()
-    .then(savedBlog => {
-      response.json(savedBlog)
-    })
-    .catch(error => next(error))
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -29,12 +29,37 @@ test('correct amount of blogs are returned as json', async () => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
-test.only('unique identifier of the blog posts is named id', async () => {
+// Verify that the default database identifier _id field is renamed to id
+test('unique identifier of the blog posts is named id', async () => {
   const response = await api.get('/api/blogs')
   response.body.forEach((blog) => {
     assert.strictEqual(blog._id, undefined)
     assert.ok(blog.id)
   })
+})
+
+// Verify that a blog can be added with POST
+test('a blog can successfully be added ', async () => {
+  const newBlog =   {
+    title: 'Temporary test blog',
+    author: 'Great Tester',
+    url: 'https://temporarytest777.com',
+    likes: 15
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  // Verify that the number of blogs has increased by one
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  // Verify that the new blog is among the returned blogs
+  const contents = blogsAtEnd.map(n => n.title)
+  assert(contents.includes('Temporary test blog'))
 })
 
 after(async () => {
