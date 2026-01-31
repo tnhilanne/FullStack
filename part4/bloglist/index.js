079c2f10de65f84db36b415c1c469b48a48772ab
tnhilanne <nikolas.developer@outlook.com>
2026-01-31 12:15:03 +0200

    Blog list app, exer 4.1

---
+++ part4/bloglist/index.js
@@ -0,0 +1,47 @@
+console.log('hello world')
+
+require('dotenv').config() //
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI //
+mongoose.connect(mongoUrl, { family: 4 })
+//   .then(result => {
+//     console.log('connected to MongoDB')
+//   })
+//   .catch(error => {
+//     console.log('error connecting to MongoDB:', error.message)
+//   })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003 // process.env.PORT || 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
