bc705dd1d892499e60227cba7840d0cdc3f3366a
tnhilanne <nikolas.developer@outlook.com>
2026-01-31 14:22:13 +0200

    Exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -7,4 +7,17 @@ const totalLikes = (blogs) => {
 	return blogs.reduce((sum, blog) => sum + (blog.likes || 0), 0)
 }
 
-module.exports = { dummy, totalLikes}
+const favoriteBlog = (blogs) => {
+	if (!blogs || blogs.length === 0) return null
+
+	const fav = blogs.reduce((favorite, blog) => (favorite.likes >= blog.likes ? favorite : blog))
+
+	return {
+		title: fav.title,
+		author: fav.author,
+        url: fav.url,
+		likes: fav.likes
+	}
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
