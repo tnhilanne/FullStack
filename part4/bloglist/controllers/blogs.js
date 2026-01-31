a56eb7c1b98c4448869930f2b68a61e4e4cb7730
tnhilanne <nikolas.developer@outlook.com>
2026-01-31 20:52:33 +0200

    Exercise 4.15, 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,10 +3,12 @@ Event handlers of routes are referred to as controllers */
 
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-// GET all blogs with async/await
+// GET all blogs with async/await and populate user info
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+  .find({}).populate('user', { username: 1, name: 1 })
   //console.log(blogs)
   response.json(blogs)
 })
@@ -15,14 +17,22 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -4,10 +4,14 @@ const mongoose = require('mongoose')
 
 // Define blog schema
 const blogSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: {type: String, required: true },
   author: { type: String, },
   url: { type: String, required: true },
   likes: { type: Number, default: 0 },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
